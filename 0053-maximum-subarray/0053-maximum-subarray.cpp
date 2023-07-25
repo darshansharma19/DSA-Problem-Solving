35eba577056542df66e3c7b59dcb3e505babdd57
Darshan Sharma <113374022+darshansharma19@users.noreply.github.com>
2023-07-26 00:48:22 +0530

    Time: 111 ms (92.62%), Space: 67.8 MB (33.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-//         kadane's ALgorithm
-        
         int n = nums.size();
         int sum = 0;
         int maxi = nums[0];
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i<n; i++){
             sum = sum + nums[i];
-            maxi = max(maxi,sum);
+            maxi = max(maxi, sum);
             
-            if(sum < 0){
+            if(sum<0){
                 sum = 0;
             }
-            
         }
-
-       return maxi; 
+        return maxi;
     }
 };
