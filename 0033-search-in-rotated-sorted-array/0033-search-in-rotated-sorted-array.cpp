899e321922614a1e97d06caf3917031befd09d7f
Darshan Sharma <113374022+darshansharma19@users.noreply.github.com>
2023-12-24 17:30:46 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (15.40%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,14 +4,36 @@ public:
         
         int n = nums.size();
         
-        for(int i = 0; i < n; i++){
-            if(target == nums[i]){
-                return i;
+        int low = 0;
+        int high = n-1;
+        
+        
+        while(low <= high){
+            
+            int mid = (low + high)/2;
+            
+            if(nums[mid] == target)
+            {
+                return mid;
             }
+            
+            else if(nums[low] <= nums[mid])
+            {   if(nums[low] <= target && target <= nums[mid])
+                high = mid - 1;
+             
+             else 
+                 low = mid + 1;
+            }
+            
+            else {
+               if(nums[mid] <= target && target <= nums[high])
+                low = mid + 1;
+                
+                else 
+                    high = mid - 1;
+        }
         }
         
         return -1;
-        
-        
     }
 };
